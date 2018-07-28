448d087a81a775c400e7ee512692d8fc5c54f6f1
Matthew Sit <msit@berkeley.edu>
2018-07-28 14:11:48 -0500

    fade out tabs on mobile

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,16 +1,25 @@
+var collapse_handle;
+
 function showTabs() {
+  clearTimeout(collapse_handle);
   if (isExpanded) {
     document.getElementsByClassName('header')[0].style.height = "15vw";
     if (document.getElementsByClassName('header-spacer')[0]) {
       document.getElementsByClassName('header-spacer')[0].style.height = "15vw";
     }
-    document.getElementsByClassName('tab_container')[0].style.display = "none";
+    document.getElementsByClassName('tab_container')[0].style.right = "-20vw";
+    document.getElementsByClassName('tab_container')[0].style.opacity = "0";
+    collapse_handle = setTimeout(function(){
+      document.getElementsByClassName('tab_container')[0].style.width = "0";
+    }, 2000);
   } else {
     document.getElementsByClassName('header')[0].style.height = "35vw";
     if (document.getElementsByClassName('header-spacer')[0]) {
       document.getElementsByClassName('header-spacer')[0].style.height = "35vw";
     }
-    document.getElementsByClassName('tab_container')[0].style.display = "inherit";
+    document.getElementsByClassName('tab_container')[0].style.width = "auto";
+    document.getElementsByClassName('tab_container')[0].style.right = "4vw";
+    document.getElementsByClassName('tab_container')[0].style.opacity = "1";
   }
   isExpanded = !isExpanded;
 }
