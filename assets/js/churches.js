74a7655ecbc77f5fd055dfa351d84df4b3bc8ed4
Matthew Sit <msit@berkeley.edu>
2018-08-08 18:56:14 -0500

    fix church links

--- assets/js/churches.js
+++ assets/js/churches.js
@@ -10,7 +10,7 @@ function readData() {
     //Loop through each news data row and format/display it.
     for (var i = 4; i < data.length; i+=4) {
       htmlExpr += "<div class=\"church-block\">";
-      htmlExpr += "<a href=\"" + data[i+1] + "\" target=\"_blank\">";
+      htmlExpr += "<a href=\"" + data[i+1]["gs$cell"]["$t"] + "\" target=\"_blank\">";
       htmlExpr += "<div class=\"church-img\"><img ";
       if (data[i+2]["gs$cell"]["$t"] != "--") {
         htmlExpr += "src=\"" + data[i+2]["gs$cell"]["$t"] + "\"></div>";
